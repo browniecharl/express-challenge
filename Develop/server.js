111745c58863dc5e369b879dd5725f6f2bb9c6af
browniecharl <99853094+browniecharl@users.noreply.github.com>
2022-07-21 17:17:48 -0400

    got get routes to work, messing with bug displaying content to page

---
+++ Develop/server.js
@@ -0,0 +1,47 @@
+// const PORT = process.env.PORT || 3001;
+const express = require("express");
+const fs = require("fs");
+const path = require("path");
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "/public/index.html"));
+});
+
+app.get("/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "/public/notes.html"));
+});
+
+app.get("/api/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "/db/db.json"));
+});
+
+app.get("/api/notes/:id", (req, res) => {
+  let currentNote = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+  res.json(currentNote[Number(req.params.id)]);
+});
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "/public/index.html"));
+});
+
+app.post("/api/notes", (req, res) => {
+  let currentNote = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+  let newNote = req.body;
+  let noteID = currentNote.length.toString();
+
+  newNote.id = noteID;
+  currentNote.push(newNote);
+
+  fs.writeFileSync("./db/db.json", JSON.stringify(currentNote));
+  console.log("Note has been saved. Note info: ", newNote);
+  res.json(currentNote);
+});
+
+app.listen(3001, () => {
+  console.log(`Now running server on port 3001`);
+});
