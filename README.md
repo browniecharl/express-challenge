929a9b5c3be4ce800aac039bf0078540d4c43431
browniecharl <99853094+browniecharl@users.noreply.github.com>
2022-07-21 18:48:56 -0400

    added comments to server.js

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 ## Description:
 
-This is an application that allows users to organize thoughts and keep track of tasks they need to complete.
+This is an application was inspired to allow users to organize their thoughts and keep track of any tasks they need to complete.
 
 ## Built With
 
--- server.js
+++ server.js
@@ -5,30 +5,44 @@ const path = require("path");
 
 const app = express();
 
+// Middleware
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.use(express.json());
+
+// This route will get the homepage and display to the browser
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "/public/index.html"));
 });
 
+//// This route will get the 'notes' page and display to the browser
+
 app.get("/notes", (req, res) => {
   res.sendFile(path.join(__dirname, "/public/notes.html"));
 });
 
+// This route will get the note data from the db.json and send it to the 'notes' page
+
 app.get("/api/notes", (req, res) => {
   res.sendFile(path.join(__dirname, "/db/db.json"));
 });
+
+// This route gets a single note by ID
 
 app.get("/api/notes/:id", (req, res) => {
   let currentNote = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
   res.json(currentNote[Number(req.params.id)]);
 });
 
+// This route links to the homepage if there is an error in an API call
+
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "/public/index.html"));
 });
+
+// This route is able to create a new note and post it to the db.json
 
 app.post("/api/notes", (req, res) => {
   let currentNote = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
@@ -42,6 +56,8 @@ app.post("/api/notes", (req, res) => {
   console.log("Note has been saved. Note info: ", newNote);
   res.json(currentNote);
 });
+
+// This route makes it possible to delete a single note
 
 app.delete("/api/notes/:id", (req, res) => {
   let currentNote = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
@@ -60,6 +76,8 @@ app.delete("/api/notes/:id", (req, res) => {
   fs.writeFileSync("./db/db.json", JSON.stringify(currentNote));
   res.json(currentNote);
 });
+
+// This wil listen for the server to start and log to the console that it is now running
 
 app.listen(PORT, () => {
   console.log(`Now running server on port ${PORT}`);
